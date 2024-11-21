e13e600c7a6f2ee9e2c5d595097e442d8226f6a0
deepankjadav <jadavdeepank@gmail.com>
2024-11-21 21:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit" ,function(event){
+    event.preventDefault;
+
+    let age = document.getElementById("age").value;
+    let name = document.getElementById("name").value;
+
+    if(!name || !age){
+        alert("inputs cannot be empty.");
+        return;
+    }
+
+    age=parseInt(age ,10);
+    const checkpromise = new Promise((resolve , reject) =>
+    {
+        setTimeout(() => {
+            if(age => 18){
+                resolve(`Welcome, ${name}. You can vote.`);
+            }
+            else{
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+
+    checkpromise
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+
+});
